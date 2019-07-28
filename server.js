3eb166e6f19bcd5178781ea68a28e6032b6df9fa
adilbeq <baktiyarovadilbek@gmail.com>
2019-07-28 23:10:31 +0600

    login to portal

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
