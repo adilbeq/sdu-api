04e2b74dd338aae244d48371d1d19f2b755fd07d
adilbeq <baktiyarovadilbek@gmail.com>
2019-07-28 23:09:52 +0600

    login to portal

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
 app.post('/grades', function (req, res) {
   const puppeteer = require('puppeteer');
-
   (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://my.sdu.edu.kz/index.php', {waitUntil: 'networkidle2', headless: false});
-    await page.pdf({path: 'hn.pdf', format: 'A4'});
+    await page.goto('https://my.sdu.edu.kz/index.php', {waitUntil: 'networkidle2', headless: true});
+    await page.waitForSelector('#username');
+    const username = '#username';
+    await page.$eval(username, (el, value) => el.value = value, req.body.username);
+    const password = '#password';
+    await page.$eval(password, (el, value) => el.value = value, req.body.password);
+      
+    const submitBtn = "div > form > input";
+    await page.click(submitBtn);
+    await page.screenshot({path: 'clickbd.png',fullPage: true});
 
     await browser.close();
+    res.send(200);
   })();
-
 });
 
 app.listen(3000, function () {
